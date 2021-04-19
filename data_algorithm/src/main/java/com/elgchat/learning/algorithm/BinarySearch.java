75037bb399b6a7d8b467e927e4af6b7b3e5667b5
Jani <jianghai@tom.com>
2021-04-19 15:57:19 +0800

    二分查找法实现

--- data_algorithm/src/main/java/com/elgchat/learning/algorithm/BinarySearch.java
+++ data_algorithm/src/main/java/com/elgchat/learning/algorithm/BinarySearch.java
@@ -28,10 +28,37 @@ public class BinarySearch {
         return -1;
     }
 
+    private static int recursiveSearch(int[] nums, int num, int low, int height) {
+
+        int mind = (low + height) / 2;
+        if (nums[mind] == num) {
+            return mind;
+        }
+        if (num < nums[mind]) {
+            height = mind - 1;
+        }
+        if (num > nums[mind]) {
+            low = mind + 1;
+        }
+
+        if (low == height) {
+            return -1;
+        }
+        return recursiveSearch(nums, num, low, height);
+    }
+
+    private static int bruteForce(int[] nums1) {
+
+        return 0;
+    }
+
+
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 11, 12, 13, 22, 23, 24, 33, 34, 35, 36};
         System.out.println(binarySearch(nums, 24));
+        System.out.println(recursiveSearch(nums, 12, 0, nums.length - 1));
 
-        System.out.println(binarySearch(nums, 14));
+        int[] nums1 = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 8, 8, 9, 9, 11, 11};
+        System.out.println(bruteForce(nums1));
     }
 }
